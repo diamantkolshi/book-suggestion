a475edeb93815b30d46a9a043ddfc64ce014a008
Diamant <diamant@Diamants-MacBook-Air.local>
2016-08-13 01:02:26 +0200

    rails admin configuration and authenticate, added categories table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ActiveRecord::Base
+  
+	has_attached_file :avatar, styles: { medium: "250x250#", thumb: "54x54#", profile: '386x244#' }
+	validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
--- db/migrate/20160811203310_create_items.rb
+++ db/migrate/20160811203310_create_items.rb
@@ -1,15 +1,16 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.string 			:firstname
-      t.string 			:lastname
-      t.attachment 	:avatar
-      t.string 			:email
-      t.text   			:description
-      t.string 			:address
-      t.integer 		:phone
-      t.integer   	:code
-      t.boolean			:pending 
+      t.string      :firstname
+      t.string      :lastname
+      t.attachment  :avatar
+      t.string      :email
+      t.text        :description
+      t.string      :address
+      t.string      :phone
+      t.string      :code
+      t.boolean     :pending
+      t.boolean     :approve
 
       t.timestamps null: false
     end
