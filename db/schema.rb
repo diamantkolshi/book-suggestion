a8982034f08653d9a580f20b62c08ecc9edaefc7
Dardan <dardanlladrovci@gmail.com>
2016-09-14 11:41:32 +0200

    Switched database to pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813055941) do
+ActiveRecord::Schema.define(version: 20160820044025) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "access_codes", force: :cascade do |t|
+    t.string   "al_code"
+    t.string   "ks_code"
+    t.string   "registration_code"
+    t.boolean  "status",            default: true
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -19,24 +31,24 @@ ActiveRecord::Schema.define(version: 20160813055941) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "item_categories", force: :cascade do |t|
-    t.integer  "item_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "prefix"
+    t.integer  "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "item_categories", ["category_id"], name: "index_item_categories_on_category_id"
-  add_index "item_categories", ["item_id"], name: "index_item_categories_on_item_id"
-
   create_table "items", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
+    t.integer  "category_id"
+    t.integer  "country_id"
+    t.string   "name"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "email"
+    t.string   "location"
     t.text     "title"
     t.text     "description"
     t.decimal  "price"
@@ -48,5 +60,8 @@ ActiveRecord::Schema.define(version: 20160813055941) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
   end
+
+  add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
+  add_index "items", ["country_id"], name: "index_items_on_country_id", using: :btree
 
 end
